61f4fc56e5935c6434444f8fcca2624032f23b04
khoirulanwar7 <muhammadkhoirulanwarudin7.gmail.com>
2022-11-23 23:35:13 +0700

    Commit

--- resources/views/admin/base/base.blade.php
+++ resources/views/admin/base/base.blade.php
@@ -28,11 +28,12 @@
         <img class="animation__shake" src="{{ url ('img/logo.png') }}" alt="AdminLTELogo" height="" width="200">
       </div>
 
-      <nav class="main-header navbar navbar-expand navbar-white navbar-light dekorasi p-4">
+      <nav class="main-header navbar navbar-expand navbar-white navbar-light dekorasi p-2">
         <ul class="navbar-nav">
-          <li class="nav-item">
-          </li>
-        </ul>
+      <li class="nav-item">
+        <a class="nav-link text-white" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+    </ul>
       </nav>
 
       <aside class="main-sidebar sidebar-dark-primary elevation-4 dekorasi">
