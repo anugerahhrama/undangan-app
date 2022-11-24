a636023cf6654cdfa245186fc7bd0eb474011b15
AnugerahR08 <69343981+AnugerahR08@users.noreply.github.com>
2022-11-24 10:18:39 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -38,3 +38,43 @@ Route::group(['middleware' => ['auth']], function() {
         });
     });
 });
+
+Route::get('/', function () {
+    return view('userPage');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/admin', function () {
+    return view('admin/dashboard');
+});
+
+Route::get('/data-user', function () {
+    return view('admin/dataUser');
+});
+
+Route::get('/data-admin', function () {
+    return view('admin/dataAdmin');
+});
+
+Route::get('/tambah-user', function () {
+    return view('admin/formUser');
+});
+
+Route::get('/tambah-admin', function () {
+    return view('admin/formAdmin');
+});
+
+Route::get('/edit-user', function () {
+    return view('admin/editUser');
+});
+
+Route::get('/edit-admin', function () {
+    return view('admin/editAdmin');
+});
