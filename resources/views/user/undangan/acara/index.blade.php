9e00c564d22e4feca50db0ee8dbf86c02b0a3a12
khoirulanwar7 <muhammadkhoirulanwarudin7.gmail.com>
2022-12-05 15:35:24 +0700

    commit

--- resources/views/user/undangan/acara/index.blade.php
+++ resources/views/user/undangan/acara/index.blade.php
@@ -53,14 +53,11 @@
                                     <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>
                                 </button>        
                             </a>
-<<<<<<< HEAD
                           </div>
                           <div class="mx-auto">
-=======
                         </div>
                         <div>
                             <form action="{{ route('hapus_acara', ['id' => $datas->id, 'id_user' => $data->id]) }}" method="POST">
->>>>>>> 6991376359ed30cb08286eabb7141800520c57e6
                             @csrf
                             @method('DELETE')
                                 <input type="hidden" name="_method" value="DELETE">
@@ -72,7 +69,7 @@
                     </div>
                   </td>
                 </tr>
-                 @empty
+                @empty
                     <div class="p-4 mb-4 text-sm text-yellow-700 bg-yellow-100 rounded-lg dark:bg-yellow-200 dark:text-yellow-800" role="alert">
                       <h1>tidak ada data</h1>
                     </div>
--- resources/views/user/undangan/tema/1.blade.php
+++ resources/views/user/undangan/tema/1.blade.php
@@ -60,7 +60,7 @@
       <div class="flex flex-wrap md:mx-20">
         <div class="self-center px-4 w-full lg:w-1/2">
           <h1 class="text-base font-semibold text-cyan-500 md:text-2xl">
-              Undangan {{ $datas->kategori }}
+              Undangan {{ $data->kategori }}
             <span class="block text-3xl text-slate-800 font-bold md:text-5xl mb-3">
               {{ $data->judul_acara }}
             </span>
@@ -93,7 +93,7 @@
           </h2>
           <div class="w-1/3 mx-auto">
           @foreach ($acara as $ac)
-            <ul class="list-disc text-start">
+            <ul class="">
               <li class="font-semibold text-3xl text-slate-500 md:text-lg">
                 {{ $ac->acara }}
               </li>
